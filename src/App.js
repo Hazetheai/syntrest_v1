9b4f27b37def263f1611daf3505600293b3d85ed
Jake <jacobstefan92@gmail.com>
2019-06-08 11:04:52 -0400

    replaced vanilla with React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="card">
+          <div className="card__nav">
+            <button className="card__btn--more btn btn--more">&#8942;</button>
+            <button className="card__btn--share btn btn--share">Share</button>
+            <button className="card__btn--save btn btn--save">Save</button>
+          </div>
+          <div className="card__image">
+            <button className="card__btn--fullscreen btn btn--fullscreen">
+              fullscreen
+            </button>
+            <img src="" alt="" srcset="" />
+          </div>
+          <div className="card__comment">
+            <input
+              className="comment comment--input card__comment--input"
+              type="text"
+              name="comment"
+              placeholder="Write a comment"
+            />
+            <p className="comment comment--previous card__comment">
+              A Previous comment
+            </p>
+          </div>
+        </div>
       </header>
     </div>
   );
