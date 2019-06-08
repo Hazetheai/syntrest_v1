be48b92e46c42387f60d6e81505b07efc12d71ae
Jake <jacobstefan92@gmail.com>
2019-06-08 19:14:50 -0400

    converted html to jsx

--- src/App.js
+++ src/App.js
@@ -1,36 +1,25 @@
 import React from "react";
-import logo from "./logo.svg";
+import { ShareBtn } from "./components/buttons/shareBtn";
+import { SaveBtn } from "./components/buttons/saveBtn";
+import { MoreBtn } from "./components/buttons/moreBtn";
+import { CardNav as Nav } from "./components/nav/cardNav";
+import { Comment } from "./components/cards/comment";
+import CardImg from "./components/cards/cardImage";
+import SoloCard from "./components/cards/card";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <div className="card">
-          <div className="card__nav">
-            <button className="card__btn--more btn btn--more">&#8942;</button>
-            <button className="card__btn--share btn btn--share">Share</button>
-            <button className="card__btn--save btn btn--save">Save</button>
-          </div>
-          <div className="card__image">
-            <button className="card__btn--fullscreen btn btn--fullscreen">
-              fullscreen
-            </button>
-            <img src="" alt="" srcset="" />
-          </div>
-          <div className="card__comment">
-            <input
-              className="comment comment--input card__comment--input"
-              type="text"
-              name="comment"
-              placeholder="Write a comment"
-            />
-            <p className="comment comment--previous card__comment">
-              A Previous comment
-            </p>
-          </div>
-        </div>
-      </header>
+      <SoloCard>
+        <Nav>
+          <MoreBtn text="More" />
+          <ShareBtn text="Share" />
+          <SaveBtn text="Save" />
+        </Nav>
+        <CardImg />
+        <Comment prevComment="A previous comment" />
+      </SoloCard>
     </div>
   );
 }
