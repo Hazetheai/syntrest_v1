4278a1c511e49b02f00e2c20621f039885a94782
Jake <jacobstefan92@gmail.com>
2019-06-10 09:42:00 -0400

    added component skeletons for masonry

---
+++ src/components/layouts/home.js
@@ -0,0 +1,15 @@
+/** @jsx jsx */
+import { jsx } from "@emotion/core";
+import React, { Component } from "react";
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div>
+        <section className="masonry-container">
+          <div className="masonry">Masonry</div>
+        </section>
+      </div>
+    );
+  }
+}
