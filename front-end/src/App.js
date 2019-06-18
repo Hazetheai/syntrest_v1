a0562d1f0b73cc53a0f5fcab002df4f7a6765831
Jake <jacobstefan92@gmail.com>
2019-06-18 19:57:30 -0400

    integrated basic reactRouter to app

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,4 +1,9 @@
 import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
+
 import { ShareBtn } from "./components/buttons/ShareBtn";
 import { SaveBtn } from "./components/buttons/SaveBtn";
 import { MoreBtn } from "./components/buttons/MoreBtn";
@@ -8,36 +13,41 @@ import { Comment } from "./components/cards/Comment";
 import CardImg from "./components/cards/CardImage";
 import SoloCard from "./components/cards/Card";
 import Home from "./components/layouts/Home";
+import Landing from "./components/layouts/Landing";
+
 import MainNav from "./components/nav/MainNav";
 import Header from "./components/header/Header";
 import Search from "./components/header/Search";
-import Login from "./components/layouts/Login";
-import Signup from "./components/layouts/Signup";
-import "./App.css";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
 
 function App() {
   return (
-    <div className="App">
-      <Header>
-        {/* <HomeBtn /> */}
-        <Search />
-        <MainNav />
-      </Header>
-
-      <Home />
-      <SoloCard>
-        <Nav>
-          <MoreBtn text="More" />
-          <ShareBtn text="Share" />
-          <SaveBtn text="Save" />
-        </Nav>
-        <CardImg />
-        <Comment prevComment="A previous comment" />
-      </SoloCard>
-
-      <Signup />
-      <Login />
-    </div>
+    <Provider>
+      <Router>
+        {" "}
+        <div className="App">
+          <Header>
+            <HomeBtn />
+            <Search />
+            <MainNav />
+          </Header>
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+          <Home />
+          <SoloCard>
+            <Nav>
+              <MoreBtn text="More" />
+              <ShareBtn text="Share" />
+              <SaveBtn text="Save" />
+            </Nav>
+            <CardImg />
+            <Comment />
+          </SoloCard>
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
