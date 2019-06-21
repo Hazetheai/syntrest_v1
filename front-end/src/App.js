ddf78370c4ee6288d6b8a0d0e827e6a1c0653925
Jake <jacobstefan92@gmail.com>
2019-06-20 22:53:21 -0400

    added libs in app & localstorage

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,8 +1,11 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store";
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./utils/setAuthToken";
+import { setCurrentUser, logoutUser } from "./actions/authActions";
 
 import { ShareBtn } from "./components/buttons/ShareBtn";
 import { SaveBtn } from "./components/buttons/SaveBtn";
@@ -12,7 +15,7 @@ import { CardNav as Nav } from "./components/nav/CardNav";
 import { Comment } from "./components/cards/Comment";
 import CardImg from "./components/cards/CardImage";
 import SoloCard from "./components/cards/Card";
-import Home from "./components/layouts/Home";
+import Home from "./components/home/Home";
 import Landing from "./components/layouts/Landing";
 
 import MainNav from "./components/nav/MainNav";
@@ -20,6 +23,25 @@ import Header from "./components/header/Header";
 import Search from "./components/header/Search";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
+import PrivateRoute from "./components/private-routes/PrivateRoute";
+// Check for token to keep user logged in
+if (localStorage.jwtToken) {
+  // Set auth token header auth
+  const token = localStorage.jwtToken;
+  setAuthToken(token);
+  // Decode token and get user info and exp
+  const decoded = jwt_decode(token);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+  // Check for expired token
+  const currentTime = Date.now() / 1000; // to get in milliseconds
+  if (decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser());
+    // Redirect to login
+    window.location.href = "./login";
+  }
+}
 
 function App() {
   return (
@@ -35,6 +57,9 @@ function App() {
           <Route exact path="/" component={Landing} />
           <Route exact path="/register" component={Register} />
           <Route exact path="/login" component={Login} />
+          <Switch>
+            <PrivateRoute exact path="/home" component={Home} />
+          </Switch>
           <Home />
           <SoloCard>
             <Nav>
