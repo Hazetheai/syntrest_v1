1bb07fb89811cf79437213b172fd1043a8b2300b
Jake <jacobstefan92@gmail.com>
2019-07-01 19:24:20 -0400

    refactored in to func components

--- front-end/src/components/layouts/MasonryBrick.js
+++ front-end/src/components/layouts/MasonryBrick.js
@@ -1,59 +1,48 @@
 /** @jsx jsx */
 import { jsx } from "@emotion/core";
 
-import { Component } from "react";
+export const MasonryBrick = () => {
+  return (
+    <div>
+      <figure
+        className="masonry__brick"
+        css={{
+          flex: "auto",
+          height: "25rem",
+          minWidth: "15rem",
+          margin: "0rem 0.8rem 0.8rem 0",
+          borderRadius: "10px",
+          overflow: "hidden",
+          boxShadow: "$box-shadow",
 
-export class MasonryBrick extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <div>
-        <figure
-          className="masonry__brick"
+          "&:nth-child(4n + 1)": {
+            width: "250px"
+          },
+          "&:nth-child(4n + 1):nth-child(4n + 2)": {
+            width: "325px"
+          },
+          "&:nth-child(4n + 1):nth-child(4n + 3)": {
+            width: "180px"
+          },
+          "&:nth-child(4n + 1):nth-child(4n + 4)": {
+            width: "380px"
+          },
+          "&:hover": {
+            opacity: "0.7"
+          }
+        }}
+      >
+        <img
+          className="masonry-image"
+          src="https://source.unsplash.com/400x300/?synth/"
+          alt=""
           css={{
-            flex: "auto",
-            height: "25rem",
-            minWidth: "15rem",
-            margin: "0rem 0.8rem 0.8rem 0",
-            borderRadius: "10px",
-            overflow: "hidden",
-            boxShadow: "$box-shadow",
-
-            "&:nth-child(4n + 1)": {
-              width: "250px"
-            },
-            "&:nth-child(4n + 1):nth-child(4n + 2)": {
-              width: "325px"
-            },
-            "&:nth-child(4n + 1):nth-child(4n + 3)": {
-              width: "180px"
-            },
-            "&:nth-child(4n + 1):nth-child(4n + 4)": {
-              width: "380px"
-            },
-            "&:hover": {
-              opacity: "0.7"
-            }
+            objectFit: "cover",
+            width: "100%",
+            height: "100%"
           }}
-          {...this.props}
-        >
-          <img
-            className="masonry-image"
-            src="https://source.unsplash.com/400x300/?synth/"
-            alt=""
-            css={{
-              objectFit: "cover",
-              width: "100%",
-              height: "100%"
-            }}
-          />
-        </figure>
-      </div>
-    );
-  }
-}
+        />
+      </figure>
+    </div>
+  );
+};
--- front-end/src/components/nav/MainNav.js
+++ front-end/src/components/nav/MainNav.js
@@ -1,37 +1,37 @@
 /** @jsx jsx */
 
 import { jsx } from "@emotion/core";
-
+import { Fragment } from "react";
+import { Link } from "react-router-dom";
 import AddSynBtn from "../buttons/AddSynBtn";
+import { Button } from "../buttons/Button";
 import ProfileBtn from "../buttons/ProfileBtn";
-
-import { Component } from "react";
-
-export default class MainNav extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      loggedIn: false
-    };
-  }
-  render() {
-    return (
-      <nav
-        className="Nav"
-        css={{
-          display: "inline-flex",
-          flexWrap: "nowrap",
-          padding: "5px",
-          margin: "0 10px",
-          width: "fit-content",
-          justifySelf: "flex-end"
-        }}
-        {...this.props}
-      >
-        <AddSynBtn text="+" />
-        <ProfileBtn text="Lair" />
-      </nav>
-    );
-  }
-}
+export const MainNav = () => {
+  return (
+    <nav
+      className="Nav"
+      css={{
+        display: "inline-flex",
+        flexWrap: "nowrap",
+        padding: "5px",
+        margin: "0 10px",
+        width: "fit-content",
+        justifySelf: "flex-end"
+      }}
+    >
+      {localStorage.synJwtToken ? (
+        <Fragment>
+          <AddSynBtn text="+" />{" "}
+          <Link to="/profile">
+            <ProfileBtn text="Lair" />
+          </Link>{" "}
+        </Fragment>
+      ) : (
+        <Link to="/login">
+          <Button text="Login" />
+        </Link>
+      )}
+      <Link />
+    </nav>
+  );
+};
