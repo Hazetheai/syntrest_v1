bfe057f58a077bf7c93b4d6b5ca7ee780c054305
Jake <jacobstefan92@gmail.com>
2019-07-05 18:06:05 -0400

    removed need for redux devtools

--- front-end/src/store.js
+++ front-end/src/store.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware, compose } from "redux";
+import { applyMiddleware, compose, createStore } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 const initialState = {};
@@ -6,9 +6,9 @@ const middleware = [thunk];
 const store = createStore(
   rootReducer,
   initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  compose(applyMiddleware(...middleware))
 );
 export default store;
+
+// To use devtools => add this to the compose func
+// window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
