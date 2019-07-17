e2f79dcd215cbc3c5bc0a7550df84aa335ed78cb
Jake <jacobstefan92@gmail.com>
2019-07-16 21:09:46 -0400

    added in JWT func

--- back-end/routes/modules/makeoAuthJwt.js
+++ back-end/routes/modules/makeoAuthJwt.js
@@ -1,25 +1,41 @@
 const jwt = require("jsonwebtoken");
 const User = require("../../models/OAuthUser");
-const { makeOauthJwt, currentUser } = require("../api/githubAuth");
+const { currentUser } = require("../api/githubAuth");
 const express = require("express");
 const app = express.Router();
+const keys = require("../../config/keys");
+
+function makeOauthJwt(user) {
+  const { name } = user;
+  const { id } = user;
+
+  const payload = {
+    id,
+    name
+  };
+
+  return jwt.sign(payload, keys.secretOrKey, {
+    expiresIn: 31556926 // 1 year in seconds
+  });
+}
 
 module.exports = app.post("/token", (req, res) => {
-  const user = currentUser[1];
-  const serverAccessToken = currentUser[0];
-  const accessToken = req.body.token;
-
-  if (serverAccessToken === accessToken) {
+  const user = currentUser.user;
+  const serverAccessToken = currentUser.accessToken;
+  const clientAccessToken = req.body.token;
+  if (serverAccessToken === clientAccessToken) {
     User.findOne({
-      id: user.id
+      id: user.id,
+      name: user.name
     })
       .then(user => {
         const token = makeOauthJwt(user);
+
         res.json({
           success: true,
           token: "Bearer " + token
         });
       })
       .catch(err => console.log("Sending back to Client", err));
-  } else res.send("Not today buster");
+  } else res.send("Tokens do not match - Not today buster");
 });
