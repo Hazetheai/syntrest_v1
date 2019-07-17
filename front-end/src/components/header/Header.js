4af11474580e1c054c17ab402b52323066538641
Jake <jacobstefan92@gmail.com>
2019-07-16 21:12:41 -0400

    integrated redux

--- front-end/src/components/header/Header.js
+++ front-end/src/components/header/Header.js
@@ -1,17 +1,25 @@
 /** @jsx jsx */
 
 import { jsx } from "@emotion/core";
-
+import PropTypes from "prop-types";
 import { Component } from "react";
-
-export class Header extends Component {
+import { connect } from "react-redux";
+import { logoutUser } from "../../actions/authActions";
+import HomeBtn from "../buttons/HomeBtn";
+import Search from "../header/Search";
+import MainNav from "../nav/MainNav";
+class Header extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      userLoggedIn: false
+    };
   }
 
   render() {
+    // const { user } = this.props.auth;
+
     return (
       <header
         css={{
@@ -24,10 +32,25 @@ export class Header extends Component {
           justifyContent: "space-between",
           alignItems: "center"
         }}
-        {...this.props}
-      />
+      >
+        <HomeBtn />
+        <Search />
+        <MainNav />
+      </header>
     );
   }
 }
 
-export default Header;
+Header.propTypes = {
+  logoutUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(
+  mapStateToProps,
+  { logoutUser }
+)(Header);
