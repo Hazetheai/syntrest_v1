97d0a562df8f8aefedf65a3b702184e8f0c97de9
Jake <jacobstefan92@gmail.com>
2019-07-17 00:24:25 -0400

    Version 0.0.1

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ app.use("/oauthjwt", makeOauthJwt);
 // Serve Static Assets in prod
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("./client"));
+  app.use(express.static("client/build"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
