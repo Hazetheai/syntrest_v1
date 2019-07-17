924d2aa130188f355bc1b09a17c5c44d2e69ba74
Jake <jacobstefan92@gmail.com>
2019-07-16 22:53:24 -0400

    init build

---
+++ front-end/build/precache-manifest.9366d6552246c419feb0eed437b5b4bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0751214f02d7113bb54d4bb65e394839",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2734eccad9afedd997b",
+    "url": "/static/css/main.f0a3d1ef.chunk.css"
+  },
+  {
+    "revision": "3b363890d388132701c0",
+    "url": "/static/js/2.e93d2753.chunk.js"
+  },
+  {
+    "revision": "e2734eccad9afedd997b",
+    "url": "/static/js/main.b2bb318c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
