23276c76dea365604d566294f4da7f550cfcfa8f
Jake <jacobstefan92@gmail.com>
2019-07-16 21:02:55 -0400

    moved JWT func to separate file

--- back-end/routes/api/githubAuth.js
+++ back-end/routes/api/githubAuth.js
@@ -2,12 +2,10 @@ const axios = require("axios");
 const express = require("express");
 const bcrypt = require("bcryptjs");
 const User = require("../../models/OAuthUser");
-const jwt = require("jsonwebtoken");
-const keys = require("../../config/keys");
 
 const app = express.Router();
 
-let currentUser = [];
+let currentUser = {};
 
 const { CLIENT_URI } = process.env;
 const { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } = process.env;
@@ -22,34 +20,34 @@ const githubAuth = app.get("/callback", (req, res, next) => {
       //  Send post req with my ID & Secret & the code
       url: `https://github.com/login/oauth/access_token?client_id=${GITHUB_CLIENT_ID}&client_secret=${GITHUB_CLIENT_SECRET}&code=${code}`,
       headers: {
-        accept: "application/json"
+        accept: `application/json`
       }
       // use Token to get user from github
     })
       .then(response => {
         const accessToken = response.data.access_token;
-        currentUser.push(accessToken);
-
+        // pass to global variable to move to different file **Need Better Method**
+        currentUser.accessToken = accessToken;
         return axios({
           method: "get",
           url: `https://api.github.com/user`,
           headers: { Authorization: `token ${accessToken}` }
         })
           .then(result => {
+            // with user info from GH-api -> direct to Loading component
             res.redirect(`${CLIENT_URI}/auth/github/callback/${accessToken}`);
             return result.data;
           })
           .catch(err => console.log(err));
       })
+      // authenticate user if in db; else make user from data
       .then(ghuser => {
         User.findOne({ id: ghuser.id })
           .then(user => {
             if (user) {
-              currentUser.push(user);
-              // Bypass this and go straight to authentication
-
-              makeOauthJwt(user);
-              console.log("Made JWT with current user");
+              // return this and go straight to authentication ->via React Router verifying params
+              //  Add to global variable to access in makeOAuthJwt
+              currentUser.user = user;
 
               return "Already authenticated with Github";
             } else {
@@ -60,8 +58,7 @@ const githubAuth = app.get("/callback", (req, res, next) => {
                 platform: "Github",
                 id: ghuser.id
               });
-              makeOauthJwt(newUser);
-              console.log("Made JWT with new user");
+
               // Hash GHuser id as password before saving in DB
               bcrypt.genSalt(10, (err, salt) => {
                 bcrypt.hash(newUser.password, salt, (err, hash) => {
@@ -80,30 +77,4 @@ const githubAuth = app.get("/callback", (req, res, next) => {
     res.redirect(`${CLIENT_URI}/register`);
   }
 });
-
-function makeOauthJwt(user) {
-  const name = user.name;
-  const id = user.id;
-
-  // Create JWT Payload
-  const payload = {
-    id,
-    name
-  };
-  // Sign token
-  const token = jwt.sign(payload, keys.secretOrKey, {
-    expiresIn: 31556926 // 1 year in seconds
-  });
-  console.log("Made JWT", token);
-  // axios({
-  //   method: "post",
-  //   url: CLIENT_URI,
-  //   data: {
-  //     success: true,
-  //     token: "Bearer " + token
-  //   }
-  // });
-  return token;
-}
-
-module.exports = { githubAuth, makeOauthJwt, currentUser };
+module.exports = { githubAuth, currentUser };
