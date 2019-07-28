e3fb2ce01768981719dc2e13d2f547de5b48d00a
Jake <jacobstefan92@gmail.com>
2019-07-28 18:37:06 -0400

    upload router

--- routes/api/fileUpload.js
+++ routes/api/fileUpload.js
@@ -1,30 +1,19 @@
 const express = require("express");
-const fileUpload = require("express-fileupload");
-const path = require("path");
+const uploadRouter = express.Router();
+const upload = require("../services/fileUpload");
 
-const app = express();
+const singleUpload = upload.single("image");
 
-app.use(fileUpload());
-
-const upload = app.post("/upload", (req, res) => {
-  if (req.files === null) {
-    return res.status(400).json({ msg: "No files uploaded" });
-  }
-  const file = req.files.file;
-
-  file.mv(
-    `${path.join(__dirname, "../../")}client/public/uploads/${file.name}`,
-    err => {
-      if (err) {
-        console.error(err);
-        return res.status(500).send(err);
-      }
-      return res.json({
-        fileName: file.name,
-        filePath: `/uploads/${file.name}`
+uploadRouter.post("/image-upload", (req, res) => {
+  singleUpload(req, res, error => {
+    if (error) {
+      res.status(422).send({
+        errors: [{ title: "File upload error", details: error.message }]
       });
     }
-  );
+
+    return res.json({ imageUrl: req.file.location });
+  });
 });
 
-module.exports = { upload };
+module.exports = uploadRouter;
