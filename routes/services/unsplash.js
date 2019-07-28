d513e81fbbcf03fb15ba6c064452f5b225280ec9
Jake <jacobstefan92@gmail.com>
2019-07-28 18:35:01 -0400

    unsplash api connection

---
+++ routes/services/unsplash.js
@@ -0,0 +1,34 @@
+global.fetch = require("node-fetch");
+const Unsplash = require("unsplash-js").default;
+const toJson = require("unsplash-js").toJson;
+const express = require("express");
+const app = express();
+
+// require syntax
+
+const unsplash = new Unsplash({
+  applicationId: process.env.UNSPLASH_CLIENT_ID,
+  secret: process.env.UNSPLASH_CLIENT_SECRET,
+  callbackUrl: process.env.UNSPLASH_CALLBACK_URL
+});
+
+const random = app.get("/unsplash", (req, res) => {
+  unsplash.photos
+    .listPhotos(req.query.start, req.query.count)
+    .then(toJson)
+    .then(json => {
+      return res.json(json);
+    });
+});
+
+const synth = app.get("/unsplash/synth", (req, res) => {
+  unsplash.search
+    .photos("synth")
+    .then(toJson)
+    .then(json => {
+      return res.json(json);
+    })
+    .catch(console.error());
+});
+
+module.exports = { random, synth };
