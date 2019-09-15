93b417adcb42817b19bf65e7e5c308a3e5467933
Jake <jacobstefan92@gmail.com>
2019-09-15 09:23:43 -0400

    copy of single - to be changed

---
+++ routes/services/multFileUpload.js
@@ -0,0 +1,36 @@
+const aws = require("aws-sdk");
+const multer = require("multer");
+const multerS3 = require("multer-s3");
+
+aws.config.update({
+  secretAccessKey: process.env.AWS_SECRET_KEY,
+  accessKeyId: process.env.AWS_CLIENT_ID,
+  region: "us-east-1"
+});
+
+const s3 = new aws.S3();
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+    cb(null, true);
+  } else {
+    cb(new Error("Invalid file type. Please use JPEG or PNG"), false);
+  }
+};
+
+const multFileUpload = multer({
+  fileFilter,
+  storage: multerS3({
+    s3,
+    bucket: "syntrest-user-uploads",
+    acl: "public-read",
+    metadata: function(req, file, cb) {
+      cb(null, { fieldName: "TESTING_META_DATA" });
+    },
+    key: function(req, file, cb) {
+      cb(null, Date.now().toString());
+    }
+  })
+});
+
+module.exports = multFileUpload;
