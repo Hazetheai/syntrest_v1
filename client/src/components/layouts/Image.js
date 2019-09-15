efd337813b32614da2a786e5c1c92455a9fdb233
Jake <jacobstefan92@gmail.com>
2019-09-15 09:21:04 -0400

    M/e

--- client/src/components/layouts/Image.js
+++ client/src/components/layouts/Image.js
@@ -1,5 +1,11 @@
-import React from "react";
+/** @jsx jsx */
+import { jsx } from "@emotion/core";
 
+const imageStyle = {
+  maxWidth: "400px"
+};
 export default function Image({ image }) {
-  return <img className="single-photo" src={image.urls.thumb} alt="random" />;
+  return (
+    <img className="single-photo" src={image} alt="random" style={imageStyle} />
+  );
 }
