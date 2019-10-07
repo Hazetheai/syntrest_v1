e6b6be860769de16d84ccabd23429562fc5b2add
Jake <jacobstefan92@gmail.com>
2019-10-06 20:56:57 -0400

    init

---
+++ client/src/components/auth/Unconfirmed.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Unconfirmed = () => {
+  return (
+    <div>
+      <h2>Thanks for registering with us!</h2>
+      <p>We've sent you an email to confirm you're not a robot.</p>
+      <p>
+        Of course, if you are a synth, that's ok. You'll just need to make an
+        email for yourself.
+      </p>
+      <p>Happy Synthing!</p>
+    </div>
+  );
+};
+
+export default Unconfirmed;
