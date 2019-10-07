142bca6c4e4e4b81812322bb76512984a62fd17c
Jake <jacobstefan92@gmail.com>
2019-10-06 20:56:38 -0400

    init

---
+++ client/src/components/auth/Confirmed.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Confirmed = () => {
+  return (
+    <div>
+      <h2>That's it! Now for the good stuff</h2>
+      <p>Hit the login button to continue</p>
+      <p>Have Fun!</p>
+    </div>
+  );
+};
+
+export default Confirmed;
