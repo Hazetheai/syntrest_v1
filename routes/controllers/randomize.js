191a29882a4199b4706efd9d73a04c53515b7193
Jake <jacobstefan92@gmail.com>
2019-10-06 20:58:41 -0400

    randomizes a chunk of a string

---
+++ routes/controllers/randomize.js
@@ -0,0 +1,9 @@
+module.exports = function randomize(str) {
+  const strArr = str.split("");
+
+  const randArr = [];
+  for (let i = 0; i < 12; i++) {
+    randArr.push(strArr[Math.floor(Math.random() * str.length)]);
+  }
+  return randArr.join("");
+};
