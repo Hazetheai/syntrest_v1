5aca827d739cdb6acf9ab055657a42a97049b2b3
Jake <jacobstefan92@gmail.com>
2019-10-06 20:56:20 -0400

    push user to unconf

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -7,7 +7,7 @@ import { GET_ERRORS, SET_CURRENT_USER, USER_LOADING } from "./types";
 export const registerUser = (userData, history) => dispatch => {
   axios
     .post("/api/users/register", userData)
-    .then(res => history.push("/login"))
+    .then(res => history.push("/unconfirmed"))
     .catch(err =>
       dispatch({
         type: GET_ERRORS,
